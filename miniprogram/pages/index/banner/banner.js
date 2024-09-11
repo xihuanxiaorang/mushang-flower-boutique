e6c954f7b8d10d3f038ee8a35a75af471af3e609
xiaorang <15019474951@163.com>
2024-09-11 21:55:27 +0800

    feat(index): 完成首页轮播图区域渲染
    包括封装轮播图组件以及自定义轮播图指示点样式

---
+++ miniprogram/pages/index/banner/banner.js
@@ -0,0 +1,39 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    /**
+     * 轮播图数据
+     */
+    bannerList: {
+      type: Array,
+      value: [],
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    /**
+     * 当前轮播图索引，默认为0
+     */
+    currentIndex: 0,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    /**
+     * 该方法会在轮播图切换到新的图片时被触发，主要功能是更新当前活动索引。
+     *
+     * @param {Object} e - 事件对象，包含轮播图切换的信息
+     */
+    onSwiperChange(e) {
+      const { current: currentIndex } = e.detail
+      this.setData({ currentIndex })
+    },
+  },
+})
