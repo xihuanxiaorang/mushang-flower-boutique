eb474c73046b30ee0b5d69fee22c9001b29c2381
xiaorang <15019474951@163.com>
2024-09-12 16:39:54 +0800

    feat(index): 生成并使用首页骨架屏

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -25,6 +25,10 @@ Page({
      * 人气推荐商品列表
      */
     hotList: [],
+    /**
+     * 加载状态标志，默认为 true，表示正在加载数据
+     */
+    loading: true,
   },
 
   /**
@@ -34,9 +38,14 @@ Page({
     this.getIndexData()
   },
 
+  /**
+   * 获取首页数据
+   */
   async getIndexData() {
+    // 调用 API 获取首页数据
     const [bannerList, categoryList, adList, guessList, hotList] = await reqIndexData()
-    this.setData({ bannerList, categoryList, adList, guessList, hotList })
+    // 设置数据到页面的 data 中，并将 loading 设置为 false，表示数据加载完成
+    this.setData({ bannerList, categoryList, adList, guessList, hotList, loading: false })
   },
 
   /**
