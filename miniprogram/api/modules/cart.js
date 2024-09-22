471dcc6a11bdf154b797889e23c5976b2947e60a
xiaorang <15019474951@163.com>
2024-09-22 19:04:26 +0800

    feat(cart): 完成购物车中控制单个或所有商品勾选状态的功能以及更新某个商品购买数量与删除某商品的功能

--- miniprogram/api/modules/cart.js
+++ miniprogram/api/modules/cart.js
@@ -21,3 +21,34 @@ export const addCart = ({ id, count, ...data }) => {
 export const getCartList = () => {
   return http.get('/cart/getCartList')
 }
+
+/**
+ * 更新单个商品的选中状态
+ *
+ * @param {number} id - 商品id
+ * @param {number} checked - 选中状态（通常为 0 或 1）
+ * @returns {Promise} - 返回一个 Promise 对象，该对象在请求成功后解析为服务器响应的数据
+ */
+export const updateCheckedStatus = (id, checked) => {
+  return http.get(`/cart/checkCart/${id}/${checked}`)
+}
+
+/**
+ * 更新所有商品的选中状态
+ *
+ * @param {number} checked - 选中状态（通常为 0 或 1）
+ * @returns {Promise} - 返回一个 Promise 对象，该对象在请求成功后解析为服务器响应的数据
+ */
+export const updateAllCheckedStatus = (checked) => {
+  return http.get(`/cart/checkAllCart/${checked}`)
+}
+
+/**
+ * 从购物车中删除某个商品。
+ *
+ * @param {number} id - 商品id
+ * @returns {Promise} - 返回一个 Promise 对象，该对象在请求成功后解析为服务器响应的数据
+ */
+export const deleteCartItem = (id) => {
+  return http.get(`/cart/delete/${id}`)
+}
--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,9 +1,11 @@
 import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
 import { userStore } from '@/store/index'
 import { cartApi } from '@/api/index'
+import { useModal, useToast } from '@/utils/extendApi'
+const computedBehavior = require('miniprogram-computed').behavior
 
 Page({
-  behaviors: [storeBindingsBehavior],
+  behaviors: [storeBindingsBehavior, computedBehavior],
 
   /**
    * 页面的初始数据
@@ -24,6 +26,20 @@ Page({
     },
   ],
 
+  computed: {
+    checked(data) {
+      const { cartList } = data
+      return cartList && cartList.length > 0 && cartList.every((item) => item.isChecked)
+    },
+    totalPrice(data) {
+      const { cartList } = data
+      if (cartList && cartList.length > 0) {
+        return cartList.filter((item) => item.isChecked).reduce((sum, item) => sum + item.price * item.count, 0)
+      }
+      return 0
+    },
+  },
+
   /**
    * 生命周期函数--监听页面显示
    */
@@ -40,5 +56,59 @@ Page({
     }
     const cartList = await cartApi.getCartList()
     this.setData({ cartList })
+  },
+
+  /**
+   * 选择或取消选择某个商品
+   * @param {Event} e 事件对象，包含商品id、索引和选中状态
+   */
+  async selectItem(e) {
+    const { id, index } = e.currentTarget.dataset
+    const checked = e.detail ? 1 : 0
+    await cartApi.updateCheckedStatus(id, checked)
+    this.setData({ [`cartList[${index}].isChecked`]: checked })
+  },
+
+  /**
+   * 全选或取消全选所有商品
+   * @param {Event} e 事件对象，包含选中状态
+   */
+  async selectAllItems(e) {
+    const checked = e.detail ? 1 : 0
+    await cartApi.updateAllCheckedStatus(checked)
+    const cartList = Object.assign([], this.data.cartList)
+    cartList.forEach((item) => (item.isChecked = checked))
+    this.setData({ cartList })
+  },
+
+  /**
+   * 更改购物车中某个商品的数量
+   *
+   * @param {Event} e 事件对象，包含商品id、索引和新旧数量
+   */
+  async changeCount(e) {
+    const { id, index, count: oldCount } = e.currentTarget.dataset
+    const newCount = e.detail
+    const diff = newCount - oldCount
+    if (diff === 0) return
+    await cartApi.addCart({ id, count: diff })
+    this.setData({ [`cartList[${index}].count`]: newCount })
+  },
+
+  /**
+   * 删除购物车中的某个商品。
+   *
+   * @param {Event} e 事件对象，包含商品id
+   */
+  async deleteItem(e) {
+    const modalRes = await useModal({
+      content: '删除后信息将无法恢复，是否确定删除？',
+    })
+    if (modalRes) {
+      const { id } = e.currentTarget.dataset
+      await cartApi.deleteCartItem(id)
+      await this.getCartList()
+      useToast({ icon: 'success', title: '删除成功' })
+    }
   },
 })
